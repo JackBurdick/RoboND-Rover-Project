3795fa8656ea113e4daa5da58a527d140b41055c
Jack Burdick <jackbburdick@gmail.com>
2017-06-15 21:36:59 -0400

    include target and obstacle values

--- code/drive_rover.py
+++ code/drive_rover.py
@@ -51,6 +51,10 @@ class RoverState():
         self.brake = 0 # Current brake value
         self.nav_angles = None # Angles of navigable terrain pixels
         self.nav_dists = None # Distances of navigable terrain pixels
+        self.obs_angles = None # Angles of obstacle pixels
+        self.obs_dists = None # Distances of obstacle pixels
+        self.tar_angles = None # Angles of target pixels
+        self.tar_dists = None # Distances of target pixels
         self.ground_truth = ground_truth_3d # Ground truth worldmap
         self.mode = 'forward' # Current mode (can be forward or stop)
         self.throttle_set = 0.2 # Throttle setting when accelerating
