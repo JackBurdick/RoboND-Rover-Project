9d02ae22906e94fa31901353c106bac0e6b8bc46
Jack Burdick <jackbburdick@gmail.com>
2017-06-15 21:37:27 -0400

    begin working with decision file

--- code/decision.py
+++ code/decision.py
@@ -1,13 +1,11 @@
 import numpy as np
 
 
-# This is where you can build a decision tree for determining throttle, brake and steer 
+# build a decision tree for determining throttle, brake and steer 
 # commands based on the output of the perception_step() function
 def decision_step(Rover):
 
     # Implement conditionals to decide what to do given perception data
-    # Here you're all set up with some basic functionality but you'll need to
-    # improve on this decision tree to do a good job of navigating autonomously!
 
     # Example:
     # Check if we have vision data to make decisions with
@@ -66,6 +64,16 @@ def decision_step(Rover):
         Rover.throttle = Rover.throttle_set
         Rover.steer = 0
         Rover.brake = 0
+
+    if Rover.near_sample > 0:
+        # Set mode to "stop" and hit the brakes!
+        Rover.throttle = 0
+        # Set brake to stored brake value
+        Rover.brake = Rover.brake_set
+        Rover.steer = 0
+        Rover.mode = 'stop'
+        Rover.send_pickup = True
+    
 
     return Rover
 
